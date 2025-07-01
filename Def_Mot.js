594cbff4a9920d215ecf9bbfc520ec4b78e01c68
pokegustavor <pokegustavo@gmail.com>
2025-07-01 19:27:23 -0300

    Prototipo Menu Lateral
    Adicionado prototipo do menu lateral para a página main e def_mot, também feitas alterações no def_mot para seguir essas configurações (código para abrir o menu não está sendo inserido na página Main por algum motivo)

--- Def_Mot.js
+++ Def_Mot.js
@@ -1,11 +1,27 @@
 var ativo = false;
 var executando = false;
 var elementos = new Array();
+var frequencia = null;
+var deslocamento = null;
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("#Simular").addEventListener("click", alternar);
+    document.querySelector("#def_mot_freq").addEventListener("input", function() {
+        document.querySelector("#def_mot_freq_valor").textContent = document.querySelector("#def_mot_freq").value;
+    });
+    document.querySelector("#def_mot_desl").addEventListener("input", function() {
+        document.querySelector("#def_mot_desl_valor").textContent = document.querySelector("#def_mot_desl").value;
+    });
+    iniciar();
+    document.querySelector("#def_mot").addEventListener("change", function() {
+        alternar();
+    } );
 } );
 
+function iniciar()
+{
+    frequencia = document.querySelector("#def_mot_freq");
+    deslocamento = document.querySelector("#def_mot_desl");
+}
 
 function alternar()
 {
@@ -31,14 +47,16 @@ async function simulacao()
     ...document.querySelectorAll("main p"),
     ...document.querySelectorAll("main h2"),
     ...document.querySelectorAll("main h3"),
-    ...document.querySelectorAll("main h4")
+    ...document.querySelectorAll("main h4"),
+    ...document.querySelectorAll("main li")
     ];
 
     while(ativo)
     {
+        let des = deslocamento.value;
         //Mover os elementos para uma posição aleatória
-        mover(random(-15,15),random(-15,15),elementos);
-        await new Promise(resolve => setTimeout(resolve, random(50,300)));
+        mover(random(-des,des),random(-des,des),elementos);
+        await new Promise(resolve => setTimeout(resolve, frequencia.value * 1000));
     }
     //Retornar os elementos para a posição original
     mover(0,0,elementos);
--- Geral.js
+++ Geral.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".menu-toggle").addEventListener("click", alternar_menu);
+    document.querySelector("#toggleMenu").addEventListener('click', () => {
+         document.getElementById('sidebar').classList.toggle('fechado') });
 } );
 
 
